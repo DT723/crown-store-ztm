6abcaa77b9ff5649ef3e44977a9868625009c332
DT723 <davidtesno@gmail.com>
2022-05-31 16:15:24 -0400

    created user login and auth with Firebase

---
+++ crown-store-ztm/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnfInr2pC-j3faVYwav0kiPZhnksCb0P0",
+  authDomain: "crown-store-db-9d33c.firebaseapp.com",
+  projectId: "crown-store-db-9d33c",
+  storageBucket: "crown-store-db-9d33c.appspot.com",
+  messagingSenderId: "580726657879",
+  appId: "1:580726657879:web:325b9968693d42c12fa412",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  //   console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  //   console.log(userSnapshot);
+  //   console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+
+  //check if user data exists
+
+  //create / set the document with the data from userAuth in my collection
+
+  //return userDocRef
+};
